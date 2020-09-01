90e5589df82f5b03f605a5f03017768074f269d6
Katie Knish <knish.k.t@gmail.com>
2020-09-01 16:30:08 -0500

    work on routes

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 //import GoogleBtn from './GoogleBtn'
+import {NavLink} from 'react-router-dom'
 import Routes from './components/Routes'
 import Home from './components/Home'
 import Navbar from './components/navbar'
@@ -9,22 +10,28 @@ import Search from './components/Search'
 function App() {
   return (
     <React.Fragment>
+      <ul className="nav justify-content-end shadow p-3 mb-5 bg-light rounded">
+        <li className="nav-item btn btn-light mx-3">
+          <NavLink className="text-dark" to="/home">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item btn btn-light">
+          <NavLink className="text-dark" to="/jobboard">
+            Job Board
+          </NavLink>
+        </li>
+        <li className="nav-item btn btn-light mx-3">
+          <NavLink className="text-dark" to="/search">
+            Search
+          </NavLink>
+        </li>
+      </ul>
       <div className="m-3">
-        {/* <div className="row align-items-center"> */}
-        {/* <Navbar /> */}
-        {/* <div className="col">
-            <h1 className="col-8" style={{fontFamily: 'Lobster'}}>
-              One Job, Please
-            </h1>
-          </div> */}
-        {/* <div class=" float-right">
-            <GoogleBtn />
-          </div> */}
-        {/* </div> */}
         <Navbar />
         <Routes />
 
-        {/* <Home /> */}
+        <Home />
       </div>
       <footer className="row footer justify-content-center bg-dark text-light mt-1 sticky-bottom">
         <p>
