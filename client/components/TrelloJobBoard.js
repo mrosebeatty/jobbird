6c5e1ab03d9aafea0a2075a6fbf280634f1ccc9f
Katie Knish <knish.k.t@gmail.com>
2020-09-07 16:05:25 -0500

    tabs added

--- client/components/TrelloJobBoard.js
+++ client/components/TrelloJobBoard.js
@@ -42,6 +42,19 @@ class TrelloJobBoard extends React.Component {
 
     return (
       <div className="main">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className="active-tab nav-link text-dark font-weight-bold border-secondary">
+              Job Board
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-secondary">Contacts</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-secondary">Notes</a>
+          </li>
+        </ul>
         <DragDropContext onDragEnd={this.onDragEnd}>
           <div className="row">
             {lists.map(list => {
